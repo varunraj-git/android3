8e016271dc40771bf9419eea130b1aa00e0e814e
Varun <varun.raj@student.griffith.ie>
2019-12-05 16:25:19 +0000

    First Bracket

---
+++ app/src/main/java/com/example/sensors/CustomView.java
@@ -0,0 +1,27 @@
+package com.example.sensors;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
